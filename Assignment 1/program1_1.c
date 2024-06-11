17a83b0a3041587c44e26c7039b81832aa54ffe4
omnikam.24 <152396182+ompramodnikam@users.noreply.github.com>
2024-06-11 13:21:23 +0530

    Add files via upload

---
+++ Assignment 1/program1_1.c
@@ -0,0 +1,31 @@
+//Question 1 : Program to divide two numbers..
+
+#include<stdio.h>
+
+int Divide(int iNo1, int iNo2)
+{
+    int iAns = 0;
+
+    if(iNo2 == 0)
+    {
+        return -1;
+    }
+
+    iAns = iNo1/iNo2;
+
+    return iAns;
+}
+
+int main()
+{
+
+    int iValue1 = 15;
+    int iValue2 = 3;
+    int iRet = 0;
+
+    iRet = Divide(iValue1, iValue2);
+
+    printf("Division is : %d ",iRet);
+
+    return 0;
+}
